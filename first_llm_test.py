0bbfde2939058cdc17151b4d749edcdce599fb4f
Luke Gorman <116017484+lukegorman31@users.noreply.github.com>
2024-11-01 16:20:08 +0000

    commit

--- first_llm_test.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-review = "I love this product, but found the loading times to be a bit slow." 
-
-classifier = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
-
-prediction = classifier(review)
-
-print(prediction)
